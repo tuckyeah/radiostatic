15cae8a39d079c5a64cde8edc170b9f3ac5c05cf
Tucker <h.rosebrock@gmail.com>
2015-07-23 16:07:45 -0400

    added june-7.html, leopold/meetcharlie.html, and created charlie template.

---
+++ vendor/js/tomatoes.js
@@ -0,0 +1,39 @@
+( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$slideTall = $('.homeSlideTall');
+	$slideTall2 = $('.homeSlideTall2');
+	$body = $('body');
+	 
+	function adjustWindow(){
+		
+		// Init Skrollr
+		var s = skrollr.init({
+		    render: function(data) {
+		    
+		        //Debugging - Log the current scroll position.
+		        //console.log(data.curTop);
+		    }
+		});
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    $slideTall.height(winH*2);
+	    $slideTall2.height(winH*3);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    s.refresh($('.homeSlide'));
+	    
+	}
+		
+} )( jQuery );
